e851a6d98f4c771111972108f896811625ff4f14
Etienne BENDER <66954284+betb9@users.noreply.github.com>
2025-05-17 21:12:50 +0200

    feat(user): create entity
    * Add orm-pack, maker-bundle, api and form dependencies
    
    * Set conf for SQLite database
    
    * Generate new User entity
    
    * Update gitignore to ignore IDE config
    
    * Make User email unique in database
    
    * Create new generic Timestampable trait to manage create and update dates
    
    * Use Timestampable in User entity
    
    * Create new migration to create user
    
    * Rename Timestampable
    
    * Untrack .idea from git
    
    * Remove User::createdAt, User::updatedAt attributes
    
    * Add unique email for User entity
    
    * Make User entity firstName, lastName and email required
    
    * Add valid email address constraint on User entity
    
    * Disable pagination for API

---
+++ src/Entity/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\Traits\Timestampable;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+#[ApiResource]
+#[UniqueEntity(
+    fields: ['email'],
+    message: 'Cet adresse email est déjà utilisée.',
+)]
+class User
+{
+
+    use Timestampable;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: "Le prénom est obligatoire.")]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: "Le nom est obligatoire.")]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotBlank(message: "L'adresse email est obligatoire.")]
+    #[Assert\Email(message: "L'adresse email n'est pas valide.")]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
