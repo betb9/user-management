c17c6eb5eaea0e0c52b41b2b3cbb051632774823
Etienne BENDER <bender.etienne57@gmail.com>
2025-05-17 22:36:53 +0200

    fix edit form to prevent sending empty values

--- src/Form/UserForm.php
+++ src/Form/UserForm.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,18 @@ class UserForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
+            ->add('firstName', TextType::class, [
+                'required' => true,
+                'empty_data' => ''
+            ])
+            ->add('lastName', TextType::class, [
+                'required' => true,
+                'empty_data' => ''
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'empty_data' => '',
+            ])
         ;
     }
 
