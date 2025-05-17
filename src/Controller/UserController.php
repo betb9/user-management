f540b0a827432a37b77c016a515687bf9c83b15e
Etienne BENDER <bender.etienne57@gmail.com>
2025-05-17 21:48:14 +0200

    Add route to edit or create user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,5 +19,26 @@ final class UserController extends AbstractController
         $users = $em->getRepository(User::class)->findAll();
 
         return $this->render('user/index.html.twig', ['users' => $users]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Utilisateur créé.');
+            return $this->redirectToRoute('app_user_index');
+        }
+
+        return $this->render('user/new.html.twig', [
+            'form'  => $form->createView(),
+            'is_edit' => false
+        ]);
     }
 }
