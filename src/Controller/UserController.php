8a6e211e1783d96e6f50c5b959ca4336b477ccc7
Etienne BENDER <bender.etienne57@gmail.com>
2025-05-17 21:31:01 +0200

    Add default user view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/', name: 'app_user_')]
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $users = $em->getRepository(User::class)->findAll();
+
+        return $this->render('user/index.html.twig', ['users' => $users]);
     }
 }
